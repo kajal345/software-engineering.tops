c1afd7f37f3e68d8f1cd89ceaf76c8b57f64d67b
kajal345 <59306891+kajal345@users.noreply.github.com>
2023-01-07 16:15:39 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x=80,y=90,z=90;
+	cout<<x+y+z;
+	return 0;
+}
---
+++ vool.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	bool a=true;
+	bool b=false;
+	cout<<a<<"\n";
+	cout<<b<<"\n";
+	return 0;
+}
