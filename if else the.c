6d6b45d5142751ca27e2f6f80cdc0c481bfe05de
kajal345 <59306891+kajal345@users.noreply.github.com>
2023-01-31 10:47:36 +0530

    Add files via upload

---
+++ if else the.c
@@ -0,0 +1,22 @@
+conditinal statements:
+	1)if
+	2)if else
+	3)else if
+	4)nested if
+	5)switch case  
+	
+	
+	1)syntax:
+	if (condition)
+	{
+		
+		this code will be excuted;
+	}
+	2)if(condition)
+	{
+		this code will be excuted;
+	}
+	else
+	{
+		this code will be executed;
+	}
